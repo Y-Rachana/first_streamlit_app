15b8d6ebb8e813a81b4ea04e045c23ddafd17acd
Y-Rachana <128296552+Y-Rachana@users.noreply.github.com>
2023-03-24 11:43:38 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,10 +55,4 @@ if streamlit.button('Get Fruit Load List'):
    streamlit.dataframe(my_data_rows)
 
 
-#new section to display fruityvice api response
-
-fruit_choice1 = streamlit.text_input('What fruit would you like to eat?','jackfruit')
-streamlit.write('Thanks for adding ', fruit_choice1)
-
-my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
 
